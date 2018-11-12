5aec2b44f8568039a6c562403460e5763b95d5ea
Prayag Gordy <prayaggordy@gmail.com>
2018-11-12 16:15:47 -0500

    added code to scrape rating

--- scrapeURC.R
+++ scrapeURC.R
@@ -11,7 +11,6 @@ combineDetails <- function(inV) {
 }
 getDF <- function(URL) {
 	pg <- read_html(URL)
-	show(URL)
 	details <- pg %>%
 		html_nodes(xpath = "//*[@id='contentN']/table") %>% #found with inspect element's "copy xpath"
 		html_text() %>%
@@ -36,11 +35,19 @@ getDF <- function(URL) {
 		html_nodes("#contentN > h2 > a") %>%
 		html_text()
 
+	rate <- pg %>%
+		html_nodes("#contentN > div.ratingblock") %>%
+		html_text() %>%
+		substr(., gregexpr(., pattern = "Rated: ")[[1]][1], nchar(.)) %>%
+		gsub(pattern = "\\\t|\\\n|Rated: |[()]", replace = "") %>%
+		str_split(pattern = " ") %>%
+		unlist()
+
 	info <- c(details, features) %>%
 		str_split(pattern = ": ") %>%
 		ldply() %>%
-		mutate(name = rcName, park = rcLocation) %>%
-		select(name, park, cname = V1, detail = V2) %>%
+		mutate(name = rcName, park = rcLocation, rating = rate[1], votes = rate[5]) %>%
+		select(name, park, rating, votes, cname = V1, detail = V2) %>%
 		spread(cname, detail)
 
 	return(info)
@@ -48,7 +55,7 @@ getDF <- function(URL) {
 
 #use scraping package "rvest" to find all linnks from utimaterollercoaster.com
 urc <- read_html("https://www.ultimaterollercoaster.com/coasters/browse/a-to-z") %>%
-	html_nodes(".rcyIdx a") %>% #found this css selector using Chrome extension "selectorgadget"
+	html_nodes(".rcyIdx a") %>% #found this css selector using inspect element
 	html_attr("href") %>% #get links
 	magrittr::extract(. != "#top") %>%
 	paste("https://www.ultimaterollercoaster.com", ., sep = '')
